edba62e8fc1d9788a4c11a8bf9cafbb99b5b4d37
larroyo1 <arroyoluis1516@yahoo.com>
2022-04-04 12:36:40 -0600

    Create battlefield class and complete MVP

--- battlefield.py
+++ battlefield.py
@@ -1,1 +1,50 @@
+from robot import Robot 
+from dinosaur import Dinosaur 
+from weapon import Weapon
 
+class Battlefield: 
+    def __init__(self): 
+        self.robot = Robot('Dominus')
+        self.dinosaur = Dinosaur('Jimmy the Dino', 23)
+
+
+    def display_welcome(self): 
+        print('Welcome to Robots vs Dinos.')
+
+    def battle_phase(self): 
+        while self.robot.health > 0 and self.dinosaur.health > 0: 
+            self.robot.attack(self.dinosaur)
+            print(f'The robot attacked the dino! The dino now has {self.dinosaur.health} health.')
+            
+            if self.dinosaur.health <= 0:
+                break
+            else:
+                self.dinosaur.attack(self.robot)
+                print(f'The dino attacked the robot! The robot now has {self.robot.health} health.')
+
+    def display_winner(self): 
+        if self.robot.health > self.dinosaur.health: 
+            print('The robot has won the battle')
+        else: 
+            print('The dino has won the battle!')
+
+    def select_robot_weapon(self): 
+        user_input = int(input('Choose the robots weapon! Input 1 for axe, 2 for sword, 3 for a pillow, and 4 for BAZOOKA!'))
+
+        if user_input == 1: 
+            self.robot.active_weapon = Weapon('Axe', 15)
+        elif user_input == 2: 
+            self.robot.active_weapon = Weapon('Sword', 25)
+        elif user_input == 3: 
+            self.robot.active_weapon = Weapon('Pillow', 1)
+        elif user_input == 4: 
+            self.robot.active_weapon = Weapon('Bazooka', 1000) 
+
+    def run_game(self): 
+        self.display_welcome()
+        self.select_robot_weapon()
+        self.battle_phase()
+        self.display_winner()
+
+game = Battlefield()
+game.run_game()
